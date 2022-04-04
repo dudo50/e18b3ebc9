8a35d848b3dad4ff28e23c3df65117b63e770f95
Dusan Morhac <55763425+dudo50@users.noreply.github.com>
2022-04-04 22:14:55 +0200

    frontend basic startup

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import {useState, useEffect } from "react";
 import axios from "axios";
 
@@ -8,7 +9,7 @@ export default function App() {
   useEffect(()=>{
     async function getAllUsers(){
     try {
-      const users = await axios.get("http://127.0.0.1:8000/api/profiles")
+      const users = await axios.get("https://game-browser-application.herokuapp.com/api/profiles")
       console.log(users.data)
       setUsers(users.data)
     } catch (error) {
@@ -19,7 +20,12 @@ export default function App() {
   }, [])
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>Hello I am working</Text>
+      <FlatList
+      data = {users}
+      renderItem={({item})=><Text>{item.username}, {item.password}</Text>}
+      keyExtractor={item=> item.user_id}
+      />
       <StatusBar style="auto" />
     </View>
   );
